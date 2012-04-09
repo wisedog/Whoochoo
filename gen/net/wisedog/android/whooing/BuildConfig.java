d8fb5c4a527c39824d054eebd0a83d4cea522248
Jongha Kim <me@wisedog.net>
2012-04-09 23:02:34 +0900

    first commit

---
+++ gen/net/wisedog/android/whooing/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.wisedog.android.whooing;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/net/wisedog/android/whooing/Whooing.java
@@ -0,0 +1,13 @@
+package net.wisedog.android.whooing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Whooing extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
