48489bb3b8a45d6824c69612f403de28c259e2db
wisedog <me@wisedog.net>
2012-10-09 08:18:33 +0900

    temp. no meaning ful commit

--- src/net/wisedog/android/whooing/WhooingMain.java
+++ src/net/wisedog/android/whooing/WhooingMain.java
@@ -4,6 +4,8 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.wisedog.android.whooing.views.MountainGraph;
+
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
 import org.achartengine.chart.PointStyle;
@@ -238,6 +240,14 @@ public class WhooingMain extends Activity {
 	};
 	
 	public void showGraph(){
+		MountainGraph gv = new MountainGraph(this);
+		gv.initAndDraw(new float[]{4444444.0f,5555555.0f,6666666.0f}, 
+				new float[]{333333.0f,222222.0f,123456.0f});
+		LinearLayout llBody = (LinearLayout) findViewById(R.id.test_layout);
+		llBody.addView(gv);
+	}
+	
+	public void showGraph2(){
 		String[] titles = new String[] { "Air temperature" };
 	    List<double[]> x = new ArrayList<double[]>();
 	    for (int i = 0; i < titles.length; i++) {
@@ -289,92 +299,6 @@ public class WhooingMain extends Activity {
 				LinearLayout llBody = (LinearLayout) findViewById(R.id.test_layout);
 				llBody.addView(gv);
 	}
-	
-	public void showGraph1() {
-		// TODO Auto-generated method stub
-		// 표시할 수치값
-				List<double[]> values = new ArrayList<double[]>();
-				values.add(new double[] { 14, 12, 14, 15 });
-
-				/** 그래프 출력을 위한 그래픽 속성 지정객체 */
-				XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-
-				// 상단 표시 제목과 글자 크기
-				renderer.setChartTitle("2011년도 판매량");
-				renderer.setChartTitleTextSize(20);
-
-				// 분류에 대한 이름
-				String[] titles = new String[] { "월별 판매량" };
-
-				// 항목을 표시하는데 사용될 색상값
-				int[] colors = new int[] { Color.YELLOW };
-
-				// 분류명 글자 크기 및 각 색상 지정
-				renderer.setLegendTextSize(15);
-				int length = colors.length;
-				for (int i = 0; i < length; i++) {
-					SimpleSeriesRenderer r = new SimpleSeriesRenderer();
-					r.setColor(colors[i]);
-					renderer.addSeriesRenderer(r);
-				}
-
-				// X,Y축 항목이름과 글자 크기
-				renderer.setXTitle("월");
-				renderer.setYTitle("판매량");
-				renderer.setAxisTitleTextSize(12);
-
-				// 수치값 글자 크기 / X축 최소,최대값 / Y축 최소,최대값
-				renderer.setLabelsTextSize(10);
-				renderer.setXAxisMin(0.5);
-				renderer.setXAxisMax(12.5);
-				renderer.setYAxisMin(0);
-				renderer.setYAxisMax(24);
-
-				// X,Y축 라인 색상
-				renderer.setAxesColor(Color.WHITE);
-				// 상단제목, X,Y축 제목, 수치값의 글자 색상
-				renderer.setLabelsColor(Color.CYAN);
-				renderer.setBackgroundColor(Color.WHITE);
-
-				// X축의 표시 간격
-				renderer.setXLabels(1);
-				// Y축의 표시 간격
-				renderer.setYLabels(5);
-
-				// X,Y축 정렬방향
-				renderer.setXLabelsAlign(Align.LEFT);
-				renderer.setYLabelsAlign(Align.LEFT);
-				// X,Y축 스크롤 여부 ON/OFF
-				renderer.setPanEnabled(false, false);
-				// ZOOM기능 ON/OFF
-				renderer.setZoomEnabled(false, false);
-				// ZOOM 비율
-				renderer.setZoomRate(1.0f);
-				// 막대간 간격
-				renderer.setBarSpacing(0.5f);
-
-				// 설정 정보 설정
-				XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-				for (int i = 0; i < titles.length; i++) {
-					CategorySeries series = new CategorySeries(titles[i]);
-					double[] v = values.get(i);
-					int seriesLength = v.length;
-					for (int k = 0; k < seriesLength; k++) {
-						series.add(v[k]);
-					}
-					dataset.addSeries(series.toXYSeries());
-				}
-
-				// 그래프 객체 생성
-//				GraphicalView gv = ChartFactory.getBarChartView(this, dataset,
-//						renderer, Type.STACKED);
-				
-				GraphicalView gv = //ChartFactory.getLineChart(this, dataset, renderer, Type.DEFAULT);
-				ChartFactory.getLineChartView(this, dataset, renderer);
-				// 그래프를 LinearLayout에 추가
-				LinearLayout llBody = (LinearLayout) findViewById(R.id.test_layout);
-				llBody.addView(gv);
-	}	
 	 
 	 protected void setChartSettings(XYMultipleSeriesRenderer renderer, String title, String xTitle,
 		      String yTitle, double xMin, double xMax, double yMin, double yMax, int axesColor,
