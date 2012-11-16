a570ed43d1e4901d5a359351c61767ac29b126af
wisedog <wisedog@buzzni.com>
2012-11-16 13:56:29 +0900

    Enhanced AccountsSelection

--- src/net/wisedog/android/whooing/activity/AccountsSelection.java
+++ src/net/wisedog/android/whooing/activity/AccountsSelection.java
@@ -11,6 +11,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -37,26 +39,62 @@ public class AccountsSelection extends Activity {
     }
     
     private void setupUi(){
+        String mode = getIntent().getStringExtra("mode");
+        TextView textViewAssets = (TextView) findViewById(R.id.accounts_selection_title_assets);
+        TextView textViewIncome = (TextView) findViewById(R.id.accounts_selection_title_income);
+        TextView textViewExpenses = (TextView) findViewById(R.id.accounts_selection_title_expenses);
+        TextView textViewLiability = (TextView) findViewById(R.id.accounts_selection_title_liabilities);
+        if(mode.compareTo("left") == 0){
+            textViewAssets.setText(getString(R.string.accounts_assets)+"+");
+            textViewIncome.setVisibility(View.GONE);
+        }else if(mode.compareTo("right") == 0){
+            textViewExpenses.setVisibility(View.GONE);
+        }else{  //All
+            
+        }
+        
         for(int i = 0; i < mList.size(); i++){
             TextView textView = new TextView(this);
             textView.setId(DYNAMIC_VIEW_ID+i);
             
             AccountsEntity entity = mList.get(i);
             textView.setText(entity.title);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18.0f);
+            textView.setClickable(true);
+            textView.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    v.getId();
+                    
+                }
+            });
             LinearLayout layout = null;
-            if(entity.accountType.compareTo("assets") == 0){
-                layout = (LinearLayout)findViewById(R.id.accounts_selection_assets);
-                
-            }else if(entity.accountType.compareTo("liabilities") == 0){
-                layout = (LinearLayout)findViewById(R.id.accounts_selection_liabilities);
-            }else{
+            if (entity.accountType.compareTo("assets") == 0) {
+                layout = (LinearLayout) findViewById(R.id.accounts_selection_assets);
+            } else if (entity.accountType.compareTo("liabilities") == 0) {
+                layout = (LinearLayout) findViewById(R.id.accounts_selection_liabilities);
+            } else if (entity.accountType.compareTo("capital") == 0) {
+                layout = (LinearLayout) findViewById(R.id.accounts_selection_capital);
+            } else if (entity.accountType.compareTo("expenses") == 0) {
+                if (mode.equals("right") != true) {
+                    layout = (LinearLayout) findViewById(R.id.accounts_selection_expenses);
+                }else{
+                    continue;
+                }
+            } else if (entity.accountType.compareTo("income") == 0) {
+                if (mode.equals("left") != true) {
+                    layout = (LinearLayout) findViewById(R.id.accounts_selection_income);
+                }else{
+                    continue;
+                }
+            } else {
                 continue;
             }
-            layout.addView(textView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                    LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(20, 0, 0, 20);
+            textView.setLayoutParams(layoutParams);
             
-            
+            layout.addView(textView);
         }
-        
-        
     }
 }
