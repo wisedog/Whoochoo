7e552d9f64910fbb8f6aeabc4851106dc0268b4c
wisedog <wisedog@buzzni.com>
2012-11-22 11:31:07 +0900

    Add sms receiver, add permission in AndroidManifest

--- src/net/wisedog/android/whooing/sms/SmsReceiver.java
+++ src/net/wisedog/android/whooing/sms/SmsReceiver.java
@@ -6,6 +6,9 @@ package net.wisedog.android.whooing.sms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 /**
  * @author Wisedog(me@wisedog.net)
@@ -18,7 +21,22 @@ public class SmsReceiver extends BroadcastReceiver {
      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO Auto-generated method stub
+        Bundle bundle = intent.getExtras();        
+        SmsMessage[] msgs = null;
+        String str = "";            
+        if (bundle != null)
+        {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];            
+            for (int i=0; i<msgs.length; i++){
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
+                str += "SMS from " + msgs[i].getOriginatingAddress();                     
+                str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "\n";        
+            }
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
