0ea04e2587adc38c85067d73f5ef01c22c335467
wisedog <me@wisedog.net>
2012-12-20 00:34:44 +0900

    Add button in actionbar, change some resources

--- src/net/wisedog/android/whooing/activity/MainFragmentActivity.java
+++ src/net/wisedog/android/whooing/activity/MainFragmentActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
 import com.viewpagerindicator.PageIndicator;
 import com.viewpagerindicator.TitlePageIndicator;
 
@@ -32,4 +35,23 @@ public class MainFragmentActivity extends SherlockFragmentActivity {
         
         //TODO +, 메뉴, API 버튼, Progressive 버튼 추가
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add("Plus").setIcon(R.drawable.menu_plus_button_white)
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+		SubMenu subMenu1 = menu.addSubMenu("Lists");
+		subMenu1.add("Menu1");
+		subMenu1.add("Menu2");
+		subMenu1.add("Setting");
+
+		MenuItem subMenu1Item = subMenu1.getItem();
+		subMenu1Item.setIcon(R.drawable.menu_lists_button);
+		subMenu1Item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	
 }
