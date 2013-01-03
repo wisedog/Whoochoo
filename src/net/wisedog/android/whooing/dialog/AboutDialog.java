c99246a93a9912cd4f68f75acbdd26e5b37cbab3
wisedog <wisedog@buzzni.com>
2013-01-03 18:03:26 +0900

    Add dialog

---
+++ src/net/wisedog/android/whooing/dialog/AboutDialog.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package net.wisedog.android.whooing.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * @author Wisedog(me@wisedog.net)
+ *
+ */
+public class AboutDialog extends Dialog {
+
+    /**
+     * @param context
+     */
+    public AboutDialog(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/net/wisedog/android/whooing/dialog/AccountChooserDialog.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package net.wisedog.android.whooing.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * @author Wisedog(me@wisedog.net)
+ *
+ */
+public class AccountChooserDialog extends Dialog {
+
+    /**
+     * @param context
+     */
+    public AccountChooserDialog(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+}
